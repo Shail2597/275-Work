dfa2d28d536bcfc33c976ee15a47e86c067982d5
Shail Chaudhari <shail.ch259@gmail.com>
2026-02-04 13:37:50 -0700

    Completed Q2 of A1

---
+++ Assignments/a1/provided/q2/testDescribe.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ "$#" -eq 0 ]]; then
+    echo "Usage: ./testDescribe <test_set_file>" >&2
+    exit 1
+fi
+
+for stem in $(cat "$1"); do
+    echo "Description for test case '$stem':"
+    desc_file = "$stem.desc"
+    if [[ -f "$desc_file" ]]; then
+        cat "$desc_file"
+    else
+        echo "$stem: No test description available"
+    fi
+done
