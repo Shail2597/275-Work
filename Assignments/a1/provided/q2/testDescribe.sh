ac38b89221b284b8e6e535f94cf5d616eab4f01e
Shail Chaudhari <shail.ch259@gmail.com>
2026-02-05 08:32:41 -0700

    A1 Question 2 done

--- Assignments/a1/provided/q2/testDescribe.sh
+++ Assignments/a1/provided/q2/testDescribe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$#" -eq 0 ]]; then
-    echo "Usage: ./testDescribe <test_set_file>" >&2
+    echo "Usage: ./testDescribe test_set_file" >&2
     exit 1
 fi
 
---
+++ Assignments/a1/provided/q3/runInTests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ "$#" -ne 2 ]]; then
+    echo "./runInTests <command> test_set_file" >&2
+    exit 1
+fi
+
+command="$1"
+testset="$2"
+
+for stem in $(cat "$testset"); do
+    tempfile=$(mktemp)
+    $command < "${stem}.in" > "$tmpfile"
+    diff "$tmpfile" "${stem}.out" > /dev/null
+    if [[ $? -eq 0 ]]; then
+        echo "Test ${stem} passed"
+    else
+        echo "Test ${stem} failed"
+        echo "Expected output:"
+        cat "${stem}.out"
+        echo "Actual output:"
+        cat "$tmpfile"
+fi
+
+rm "$tmpfile"
