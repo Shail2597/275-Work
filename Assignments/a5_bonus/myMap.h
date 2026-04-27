a2bee5567d589719cc3adc03116999f4bc933619
Shail Chaudhari <shail.ch259@gmail.com>
2026-04-27 14:50:28 -0600

    Angad Test

--- Assignments/a5_bonus/myMap.h
+++ Assignments/a5_bonus/myMap.h
@@ -6,88 +6,104 @@
 
 template <typename K, typename V>
 class Map {
-private:
-  std::vector<std::pair<K, V>> dataStore;
+  private:
+  std::vector<std::pair<K, V>> entries;
 
-  // Returns the index of the first element >= key
-  std::size_t lowerBound(const K& key) const {
-    std::size_t startIdx = 0;
-    std::size_t endIdx = dataStore.size();
+  //helper
+  std::size_t findIndex(const K& key) const{
+    //binary search
+    std::size_t left = 0;
+    std::size_t right = entries.size();
 
-    while (startIdx < endIdx) {
-      std::size_t pivot = startIdx + (endIdx - startIdx) / 2;
-      if (dataStore[pivot].first < key) {
-        startIdx = pivot + 1;
-      } else {
-        endIdx = pivot;
+    while(left < right){
+      std::size_t mid = left + (right - left)/2;
+
+      if(entries[mid].first < key){
+        left = mid + 1;
+      }
+      else{
+        right = mid;
       }
     }
-    return startIdx;
+    return left;
   }
 
-  // Helper to check equality using only the < operator
-  bool keysEqual(const K& a, const K& b) const {
-    return !(a < b) && !(b < a);
-  }
+  //public
+  public:
 
-public:
-  Map() = default;
+  //big 5
   ~Map() = default;
-  Map(const Map&) = default;
-  Map(Map&&) = default;
-  Map& operator=(const Map&) = default;
-  Map& operator=(Map&&) = default;
+  Map(const Map& other) = default;
+  Map(Map&& other) = default;
+  Map& operator=(const Map& other) = default;
+  Map& operator=(Map&& other) = default;
+  
+  //default ctor
+  Map() = default;
 
-  V& operator[](const K& key) {
-    std::size_t insertIdx = lowerBound(key);
-    
-    if (insertIdx < dataStore.size() && keysEqual(dataStore[insertIdx].first, key)) {
-      return dataStore[insertIdx].second;
+  //operator overloading: 
+  V& operator[](const K& key){
+    std::size_t index = findIndex(key);
+
+    //if key exists
+    if (index < entries.size() &&
+        !(entries[index].first < key) &&
+        !(key < entries[index].first)) {
+        return entries[index].second;
     }
 
-    dataStore.insert(dataStore.begin() + insertIdx, std::pair<K, V>(key, V{}));
-    return dataStore[insertIdx].second;
+    //otherwise
+    entries.insert(entries.begin() + index, std::pair<K,V>(key, V{}));
+    return entries[index].second;
+
   }
 
-  V operator()(const K& key) const {
-    std::size_t insertIdx = lowerBound(key);
-    
-    if (insertIdx < dataStore.size() && keysEqual(dataStore[insertIdx].first, key)) {
-      return dataStore[insertIdx].second;
+  V operator()(const K& key) const{
+    std::size_t index = findIndex(key);
+
+    //if key exists
+    if (index < entries.size() &&
+        !(entries[index].first < key) &&
+        !(key < entries[index].first)) {
+        return entries[index].second;
     }
 
-    return V{}; 
+    //otherwise
+    return V{};
   }
-
 
   class Iterator {
-  private:
-    const Map* parentMap;
-    std::size_t iterPos;
+   private:
+    const Map* mPtr;
+    std::size_t index;
 
-  public:
-    Iterator(const Map* map, std::size_t index) : parentMap(map), iterPos(index) {}
+   public:
+   //default ctor
+    Iterator(const Map* map, std::size_t index) :
+    mPtr{map}, index{index} {}
 
-    const K& operator*() const {
-      return parentMap->dataStore[iterPos].first;
+    //isEqual check
+    bool operator!=(const Iterator& other) const{
+      return mPtr != other.mPtr || index != other.index; 
     }
 
-    Iterator& operator++() {
-      iterPos++;
+    //iterate 
+    Iterator& operator++(){
+      ++index;
       return *this;
     }
 
-    bool operator!=(const Iterator& other) const {
-      return parentMap != other.parentMap || iterPos != other.iterPos;
+    //dereference
+    const K& operator*() const{
+      return mPtr->entries[index].first;
     }
   };
 
-  Iterator begin() const {
+  Iterator begin() const{
     return Iterator(this, 0);
   }
-
-  Iterator end() const {
-    return Iterator(this, dataStore.size());
+  Iterator end() const{
+    return Iterator(this, entries.size());
   }
 };
 
